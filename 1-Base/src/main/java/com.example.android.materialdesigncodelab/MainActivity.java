fabfb93dc7bb3546399ebe2e19465d875f7dc52d
Untung Suryono <untungs30@gmail.com>
2016-09-25 16:18:20 +0700

    adding toolbar

--- 1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
