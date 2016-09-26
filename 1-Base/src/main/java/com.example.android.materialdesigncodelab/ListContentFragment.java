9954eedb7df2c9ce9be0e3b656c01eac0d5e283a
Untung Suryono <untungs30@gmail.com>
2016-09-26 10:17:40 +0700

    adding content for List RecyclerView

--- 1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/ListContentFragment.java
@@ -1,5 +1,9 @@
 package com.example.android.materialdesigncodelab;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,13 +12,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ListContentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
-        ContentAdapter adapter = new ContentAdapter();
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -23,15 +29,34 @@ public class ListContentFragment extends Fragment {
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView picture;
+        public TextView title;
+        public TextView description;
+
         public ViewHolder(LayoutInflater layoutInflater, ViewGroup parent) {
             super(layoutInflater.inflate(R.layout.item_list, parent, false));
+            picture = (ImageView) itemView.findViewById(R.id.list_avatar);
+            title = (TextView) itemView.findViewById(R.id.list_title);
+            description = (TextView) itemView.findViewById(R.id.list_desc);
         }
     }
 
     public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
         private static final int LENGTH = 18;
+        private final String[] places;
+        private final String[] placeDesc;
+        private final Drawable[] placePictures;
 
-        public ContentAdapter() {
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            places = resources.getStringArray(R.array.places);
+            placeDesc = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+            placePictures = new Drawable[a.length()];
+            for (int i = 0; i < placePictures.length; i++) {
+                placePictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
         }
 
         @Override
@@ -41,7 +66,9 @@ public class ListContentFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-
+            holder.picture.setImageDrawable(placePictures[position % placePictures.length]);
+            holder.title.setText(places[position % places.length]);
+            holder.description.setText(placeDesc[position % placeDesc.length]);
         }
 
         @Override
